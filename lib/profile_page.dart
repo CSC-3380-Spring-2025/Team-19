5ec54d52a50fc1f5c1adfbaaefd5f7080682c01e
Jonathan Breaux <jbreaux1337@gmail.com>
2025-04-07 14:43:29 -0500

    Makes Profile Page Work Now

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,22 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProfilePage(),
+    );
+  }
+}
+
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
