958b6a569a570eeca2fa613b843b599573fb534d
Jonathan Breaux <jbreaux1337@gmail.com>
2025-04-25 15:57:48 -0500

    Adds Test Entries to Leaderboard

--- lib/data/seed_letterquest.dart
+++ lib/data/seed_letterquest.dart
@@ -1,5 +1,4 @@
 import 'package:team_19/models/letterquest_model.dart'; 
-import '/db/databasehelper.dart';
 
 final letterquest1 = Letterquest(id: 1, phrase: 'UNDER THE WEATHER', hint: 'Sick');
 final letterquest2 = Letterquest(id: 2, phrase: 'SPILL THE BEANS', hint: 'Reveal');
