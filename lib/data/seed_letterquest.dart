db090347480cbe443e0a6aab25eab2bca6bc32ed
Jonathan Breaux <jbreaux1337@gmail.com>
2025-04-27 16:14:54 -0500

    More Letterquest Seed Data Changes

--- lib/data/seed_letterquest.dart
+++ lib/data/seed_letterquest.dart
@@ -1,7 +1,7 @@
 import 'package:team_19/models/letterquest_model.dart'; 
 
-final letterquest1 = Letterquest(id: 1, phrase: 'UNDER THE WEATHER', hint: 'Sick');
-final letterquest2 = Letterquest(id: 2, phrase: 'SPILL THE BEANS', hint: 'Reveal');
-final letterquest3 = Letterquest(id: 3, phrase: 'A BLESSING IN DISGUISE', hint: 'Fortune');
-final letterquest4 = Letterquest(id: 4, phrase: 'THE BALL IS IN YOUR COURT', hint: 'Decision');
-final letterquest5 = Letterquest(id: 5, phrase: 'BETTER LATE THAN NEVER', hint: 'Timing');
+final letterquest1 = Letterquest(id: 1, phrase: 'UNDER THE WEATHER', hint: 'SICK');
+final letterquest2 = Letterquest(id: 2, phrase: 'SPILL THE BEANS', hint: 'REVEAL');
+final letterquest3 = Letterquest(id: 3, phrase: 'A BLESSING IN DISGUISE', hint: 'FORTUNE');
+final letterquest4 = Letterquest(id: 4, phrase: 'THE BALL IS IN YOUR COURT', hint: 'DECISION');
+final letterquest5 = Letterquest(id: 5, phrase: 'BETTER LATE THAN NEVER', hint: 'TIMING');
