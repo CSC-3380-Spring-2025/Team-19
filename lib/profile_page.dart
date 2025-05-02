9596e1f1a5c903561e1d5a7b80a80ab05cd4df65
Jonathan Breaux <jbreaux1337@gmail.com>
2025-05-02 17:30:18 -0500

    removing comments

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -54,36 +54,6 @@ class ProfilePage extends StatelessWidget {
             ),
           ),
         ),
-
-        /*child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Feature 1"),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Feature 2"),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Feature 3"),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // TODO: Implement sign-out functionality
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.red,
-              ),
-              child: Text("Sign Out"),
-            ),
-          ],
-        ),*/
       ),
     );
   }
